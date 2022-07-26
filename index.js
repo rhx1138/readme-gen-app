31c8bb8772e984fac818ded04fb7e4d2095b42c0
rhx1138 <ryanjhendrix@gmail.com>
2022-07-26 15:14:25 -0400

    updated index.js with array of questions for user input, func to write readme, func to init app, gen markdown.js rendered license badges, func returns link and readme

--- index.js
+++ index.js
@@ -1,13 +1,76 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please describe your project."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please describe the installation process."
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please describe the usage of the project."
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please describe the contribution process."
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Please describe the tests."
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Please describe any questions."
+    },
+    {
+        type: "input",
+        name: "author",
+        message: "Please enter your name."
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email."
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please select a license.",
+        choices: ["MIT", "APACHE", "BSD", "None"]
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Success!");
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(function (data) {
+        const readMe = generateMarkdown(data);
+        writeToFile("README.md", readMe);
+    });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,91 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      return `[![License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    case "APACHE":
+      return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+    case "GPL":
+      return `[![License](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+    case "BSD":
+      return `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+    case "None":
+      return "";
+    default:
+      return "";
+  }
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      return `[MIT License](LICENSE.md)`;
+    case "APACHE":
+      return `[Apache License 2.0](LICENSE.md)`;
+    case "GPL":
+      return `[GNU General Public License v3](LICENSE.md)`;
+    case "BSD":
+      return `[BSD 3-Clause License](LICENSE.md)`;
+    case "None":
+      return "";
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  switch (license) {
+    case "MIT":
+      return `MIT License
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+Copyright (c) ${new Date().getFullYear()} ${data.author}
 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 `;
+    case "APACHE":
+      return `Apache License 2.0
+
+Copyright (c) ${new Date().getFullYear()} ${data.author}
+
+Licences for the original source code are available at:`;
 }
 
+// TODO: Create a function to generate markdown for README file 
+function generateMarkdown(data) {
+  return `# ${data.title}
+  ${renderLicenseBadge(data.license)} 
+  ${renderLicenseLink(data.license)}
+  ${data.description}
+  ${data.installation}
+  ${data.usage}
+  ${data.contributing}
+  ${data.tests}
+  ${data.questions}
+  `;
+}
+
+
 module.exports = generateMarkdown;
+}
